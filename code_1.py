3f63f0b1315bf4029af73a316b44b8c5d326f283
Stepan Isayev <140114944+isastep@users.noreply.github.com>
2024-02-05 13:29:24 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,17 @@
+    const data = generateRandomData();
+
+        console.log("Random Number: " + item);
+    const data = [];
+    }
+}
+main();
+function generateRandomData() {
+    return data;
+
+}
+function main() {
+    for (let i = 0; i < 10; i++) {
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+    data.forEach(item => {
+    });
